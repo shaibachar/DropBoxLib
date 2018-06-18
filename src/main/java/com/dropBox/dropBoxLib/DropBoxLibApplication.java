78b212d8080e34502933b3225142060d7c619d3f
shaibachar@gmail.com <shaibachar@gmail.com>
2018-06-18 18:52:00 +0300

    init

---
+++ src/main/java/com/dropBox/dropBoxLib/DropBoxLibApplication.java
@@ -0,0 +1,12 @@
+package com.dropBox.dropBoxLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropBoxLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropBoxLibApplication.class, args);
+	}
+}
