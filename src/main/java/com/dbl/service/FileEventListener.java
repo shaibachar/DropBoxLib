7f93d1d8c9ad09e5454d84d79447f20dd766842e
shai bachar <shaibachar@gmail.com>
2018-07-30 13:08:14 +0300

    add getInterestingFileFormat

--- src/main/java/com/dbl/service/FileEventListener.java
+++ src/main/java/com/dbl/service/FileEventListener.java
@@ -1,9 +1,11 @@
 package com.dbl.service;
 
+import java.util.List;
 import com.dbl.domain.message.FileMessage;
 
 public interface FileEventListener {
 
+	public List<String> getInterestingFileFormat();
 	public void fileChanged(FileMessage fileMessage);
 	
 }
